1878c6b03e647cf15428d7100bf5145f6cac115f
Simon <simonfh@users.noreply.github.com>
2017-11-17 00:51:10 +0100

    Added logging for the worker service.

--- worker/code/fetch-data.py
+++ worker/code/fetch-data.py
@@ -1,9 +1,14 @@
 import feedparser
 import json
 import time
-
+import logging
+import sys
 
 def main():
+    minutes = int(sys.argv[1])
+    # setup log
+    logging.basicConfig(filename=sys.argv[0].split('.py')[0]+'.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    
     while(True):
         with open('feeds.json','r') as fp:
             feeds = json.load(fp)['feeds']
@@ -18,7 +23,9 @@ def main():
 
         with open('/shared/data.json', 'w+') as fp:
             json.dump(d, fp)
-        time.sleep(5 * 60)
+        
+        logging.info('')
+        time.sleep(minutes * 60)
 
 if __name__ == "__main__":
     main()
